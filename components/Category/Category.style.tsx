073699f306e65b052681686e08c5eb3055315803
birat051 <biratbhattacharjee_ma@srmuniv.edu.in>
2023-07-11 19:38:14 +0530

    added category UI in home page for desktop-view

--- components/Category/Category.style.tsx
+++ components/Category/Category.style.tsx
@@ -1,11 +1,30 @@
 import styled from "styled-components";
 
 export const CategoryContainer=styled.div`
-    padding-top: 2rem;
-    padding-bottom: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
     display:flex;
-    flex-direction: column;
-    justify-content:space-between;
+    flex-direction: row;
+    justify-content:space-evenly;
     border: 0.5px solid grey;
     background-color: white;
+    overflow-x: scroll;
+`
+
+export const CategoryBlock=styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Noto+Sans:wght@300;400;500&display=swap');
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    h1{
+        font-family: 'Noto Sans',sans-serif;
+        font-size: 0.75em;
+        font-weight: 500;
+    }
+`
+
+export const CategorySpacer=styled.div`
+    height: 10px;
 `
--- components/Category/index.tsx
+++ components/Category/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { CategoryContainer } from './Category.style'
+import { CategoryBlock, CategoryContainer, CategorySpacer } from './Category.style'
 import { ProductCategory } from '@/models/product_category_model'
+import Image from 'next/image'
 
 type categoryPropType=
 {
@@ -10,7 +11,15 @@ type categoryPropType=
 function CategoryDisplay(props: categoryPropType) {
   return (
     <CategoryContainer>
-        
+      {props.category.map(function(data) {
+      return (
+        <CategoryBlock key={data.name}>
+          <Image src={data.imageUrl??''} height={50} alt={`category image for ${data.name}`} width={50}/>
+          <CategorySpacer />
+          <h1>{data.name}</h1>
+        </CategoryBlock>
+      )
+    })}
     </CategoryContainer>
   )
 }
